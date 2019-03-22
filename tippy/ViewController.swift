e48aed7cb0b060a64758a1139585d00dea36b3da
Ryan Luu <ryanluu12345@gmail.com>
2019-03-22 00:45:17 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Ryan Luu on 3/22/19.
+//  Copyright © 2019 rnluu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
